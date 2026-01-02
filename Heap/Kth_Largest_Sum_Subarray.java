9aca7eea0162e96704c53f04d32ccafc6cb65db7
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2026-01-03 01:50:07 +0530

    Solved Kth largest sum subarray

---
+++ Heap/Kth_Largest_Sum_Subarray.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static int kthLargest(int[] arr, int k) {
+        int n=arr.length;
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n;i++) {
+            int sum=0;
+            for(int j=i;j<n;j++) {
+                sum+=arr[j];
+                if(pq.size()<k) {
+                    pq.add(sum);
+                }
+                else if(sum>pq.peek()) {
+                    pq.poll();
+                    pq.add(sum);
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
