12b94550dc4fada7edcd063bef7de358ecd11394
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2026-01-26 22:57:16 +0530

    Solved Lowest Common Ancestor of a BST

---
+++ Binary_Search_Tree/Lowest_Common_Ancestor_of_a_Binary_Search_Tree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root == null) return null;
+
+       while(root!=null) {
+        if (root.val>p.val && root.val>q.val) root=root.left;
+        else if (root.val<p.val && root.val<q.val) root=root.right;
+        else break;
+       }
+       return root;
+    }
+}
