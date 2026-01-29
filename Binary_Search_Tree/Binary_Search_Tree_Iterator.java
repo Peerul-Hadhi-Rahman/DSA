f309d834cd9932bbccaa7fcf8f445333c4aa4ebb
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2026-01-29 23:43:24 +0530

    Solved BST Iterator

---
+++ Binary_Search_Tree/Binary_Search_Tree_Iterator.java
@@ -0,0 +1,23 @@
+class BSTIterator {
+    private Stack<TreeNode> st = new Stack<>();
+
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+    public int next() {
+        TreeNode node = st.pop();
+        if(node.right != null) {
+            pushAll(node.right);
+        }
+        return node.val;
+    }
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+    private void pushAll(TreeNode node) {
+        while(node!=null) {
+            st.push(node);
+            node=node.left;
+        }
+    }
+}
