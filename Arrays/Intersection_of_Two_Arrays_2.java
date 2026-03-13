5362e12a459195f5e43c628c818b1c4527226fdb
Peerul-Hadhi-Rahman <peerulrahman.sde@gmail.com>
2026-03-14 02:09:15 +0530

    Solved Common Elements in Two Arrays (Including Duplicates)

---
+++ Arrays/Intersection_of_Two_Arrays_2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map=new HashMap<>();
+
+        for(int num:nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> lis=new ArrayList<>();
+        for(int num:nums2) {
+            if(map.getOrDefault(num, 0) > 0) {
+                lis.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] res=new int[lis.size()];
+        int i=0;
+        for(int a:lis) {
+            res[i++]=a;
+        }
+        return res;
+    }
+}
